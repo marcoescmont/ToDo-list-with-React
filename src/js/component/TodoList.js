b9027204cab8804f07bb64b65b8a72861580ee50
marcoescmont <marcoescmont@gmail.com>
2021-07-13 00:58:28 +0000

    functionality done

--- src/js/component/TodoList.js
+++ src/js/component/TodoList.js
@@ -1,7 +1,52 @@
-import {React, useState} from "react"
+import React, { useState, useEffect, useRef } from "react";
+import PropTypes from "prop-types";
 
 const TodoList = () => {
+	const [tasks, setTasks] = useState(["new task"]);
 
-}
+	const inputRef = useRef(null);
 
-export default TodoList
+	useEffect(() => {
+		inputRef.current.focus();
+	});
+
+	let makeList = tasks.map((item, i) => {
+		return (
+			<li key={i}>
+				{item}
+				<button onClick={() => removeTask(i)}>X</button>
+			</li>
+		);
+	});
+
+	let newTask = event => {
+		if (event.keyCode === 13) {
+			let userInput = event.target.value;
+			const newTaskList = [...tasks, userInput];
+			setTasks(newTaskList);
+			event.target.value = "";
+		}
+	};
+
+	const removeTask = index => {
+		const removeArr = tasks.filter((item, i) => i != index);
+		setTasks(removeArr);
+	};
+
+	return (
+		<div>
+			<input
+				type="text"
+				id="fname"
+				name="fname"
+				placeholder="Add your task..."
+				onKeyDown={newTask}
+				ref={inputRef}
+			/>
+			<ul>{makeList}</ul>
+			<p>{tasks.length} items left</p>
+		</div>
+	);
+};
+
+export default TodoList;
